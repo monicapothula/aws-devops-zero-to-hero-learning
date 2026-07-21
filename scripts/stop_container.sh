0eebb13098a41fdae3442b4788dd76018e357c67
monicapothula <71058179+monicapothula@users.noreply.github.com>
2026-07-21 11:42:26 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,5 +3,5 @@ set -e
 
 # Stop the running container (if any)
 echo "Hi"
-containerid = 'docker ps |awk -f " " '{print $1}''
+containerid = $(docker ps | awk '{print $1}')
 docker rm -f $containerid

cf98c805b5bab2821c996648dbbd7b8f794283c4
monicapothula <71058179+monicapothula@users.noreply.github.com>
2026-07-21 11:58:08 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Hi"
-
-# Get ONLY container IDs without header rows (-q)
 containerid=$(docker ps -q)
-
-# Only run docker rm if there is actually a container running
 if [ -n "$containerid" ]; then
     docker rm -f $containerid
 else

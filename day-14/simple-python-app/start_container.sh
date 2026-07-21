f1cd5e9c5f2eca99f35e6909142603a44a059131
monicapothula <71058179+monicapothula@users.noreply.github.com>
2026-07-21 12:01:43 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull pothulamonica/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 pothulamonica/simple-python-flask-app
